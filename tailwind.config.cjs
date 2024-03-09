2f829be869329b9e8e75e51f25c7917e1f1d9d20
Alden Davidson <adavidson+github@protonmail.ch>
2024-03-08 20:40:54 -0800

    add forest and garden themes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,19 @@
-const daisyui = require("daisyui");
-const typography = require("@tailwindcss/typography");
+const daisyui = require('daisyui');
+const typography = require('@tailwindcss/typography');
 
 /** @type {import('tailwindcss').Config}*/
 const config = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 
-  theme: {
-    extend: {}
-  },
+	theme: {
+		extend: {}
+	},
 
-  plugins: [typography, daisyui]
+	plugins: [typography, daisyui],
+
+	daisyui: {
+		themes: ['forest', 'garden']
+	}
 };
 
 module.exports = config;
