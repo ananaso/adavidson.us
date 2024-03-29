229a4864fe18c27ab23a9f1ce0fb7ff68c42e5cb
Alden Davidson <adavidson+github@protonmail.ch>
2024-03-28 21:11:10 -0700

    add extra history to timeline, tweak styling of locations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,10 +31,10 @@
 					/>
 				</svg>
 			</div>
-			<div class="timeline-end mb-10">
+			<div class="timeline-start mb-10 md:text-end">
 				<time class="font-mono italic">Feb 2024</time>
 				<div class="text-lg font-bold text-primary">SkillBridge Internship, Software Engineer</div>
-				<div class="text-sm text-accent">Sagely</div>
+				<div class="text-sm font-semibold text-secondary">Sagely</div>
 				Growing software skills and learning how modern commercial/startup software development is accomplished.
 				Helping build a social learning platform to guide users' professional learning journeys using
 				AI to curate, organize, and contextualize content from across the internet. Working primarily
@@ -59,10 +59,12 @@
 					/>
 				</svg>
 			</div>
-			<div class="timeline-start mb-10 md:text-end">
+			<div class="timeline-end mb-10">
 				<time class="font-mono italic">Jan 2022</time>
 				<div class="text-lg font-bold text-primary">Full-stack Software Engineer</div>
-				<div class="text-sm text-accent">Section 31, Unites States Space Force</div>
+				<div class="text-sm font-semibold text-secondary">
+					Section 31, Unites States Space Force
+				</div>
 				Developed and sustained two full-stack applications for 1200+ space command and control users
 				worldwide. Utilized Test-drive development, extreme programming, user-centered design, and continuous
 				integration to ensure high quality and speedy software deliveries. Worked primarily with Kotlin,
@@ -86,12 +88,42 @@
 					/>
 				</svg>
 			</div>
-			<div class="timeline-end mb-10">
+			<div class="timeline-start mb-10 md:text-end">
 				<time class="font-mono italic">Nov 2019</time>
 				<div class="text-lg font-bold text-primary">Project Manager</div>
-				<div class="text-sm text-accent">Unites States Air Force / United States Space Force</div>
+				<div class="text-sm font-semibold text-secondary">
+					Unites States Air Force / United States Space Force
+				</div>
 				Led cross-functional team of eight to manage software projects related to the Advanced Extremely
 				High Frequency (AEHF) satellite constellation.
+			</div>
+			<hr class="bg-neutral" />
+		</li>
+		<li>
+			<hr class="bg-neutral" />
+			<div class="timeline-middle">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 20 20"
+					fill="currentColor"
+					class="h-5 w-5"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+						clip-rule="evenodd"
+					/>
+				</svg>
+			</div>
+			<div class="timeline-end mb-10">
+				<time class="font-mono italic">May 2019</time>
+				<div class="text-lg font-bold text-primary">Graduated & Commissioned</div>
+				<div class="text-sm font-semibold text-secondary">Rochester Institute of Technology</div>
+				<ul>
+					<li>Bachelor of Science, Computer Engineering</li>
+					<li>Minor, Military Studies & Leadership</li>
+					<li>2nd Lieutenant, United States Air Force</li>
+				</ul>
 			</div>
 			<hr class="bg-neutral" />
 		</li>
@@ -114,7 +146,7 @@
 			<div class="timeline-start mb-10 md:text-end">
 				<time class="font-mono italic">June 2017</time>
 				<div class="text-lg font-bold text-primary">Robot Programmer Intern</div>
-				<div class="text-sm text-accent">RT Leaders</div>
+				<div class="text-sm font-semibold text-secondary">RT Leaders</div>
 				Designed and built bespoke robotic cell battery construction machine with 5-member multidisciplinary
 				team, increasing customer safety and productivity
 			</div>
@@ -139,16 +171,17 @@
 			<div class="timeline-end mb-10">
 				<time class="font-mono italic">December 2016</time>
 				<div class="text-lg font-bold text-primary">Software Development & Research Intern</div>
-				<div class="text-sm text-accent">RIT Department of Computer Engineering</div>
+				<div class="text-sm font-semibold text-secondary">
+					RIT Department of Computer Engineering
+				</div>
 				Worked independently to research and implement networking protocols within a virtual domain for
 				realization of real-world radio use cases, using a synthesis of C++ and Python
 			</div>
-			<!-- <hr class="bg-neutral" /> -->
+			<hr class="bg-neutral" />
 		</li>
 	</ul>
 
-	<a role="button" class="btn" href=""><ScrollText />Resumé<Download class="h-4 w-4 text-info" /></a
+	<a role="button" class="btn w-fit md:self-center" href=""
+		><ScrollText />Resumé<Download class="h-4 w-4 text-info" /></a
 	>
 </section>
-
-<!-- TODO maybe put a cool transition on this and the home button -->
