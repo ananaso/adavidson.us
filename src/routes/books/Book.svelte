cb1001ee89cd3ab2f01bfb58f1378a49a8ab900d
Alden Davidson <adavidson+github@protonmail.ch>
2025-04-14 20:04:43 -0400

    refactor: rename book component and simplify

---
+++ src/routes/books/Book.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import OutLink from '$lib/components/OutLink.svelte';
+
+	export let title: string;
+	export let author: string;
+	export let series: string | undefined = '';
+	export let genre: string;
+	export let url: string;
+</script>
+
+<tr>
+	<td>
+		<OutLink {url}>{title}</OutLink>
+	</td>
+	<td>{author}</td>
+	<td>{series}</td>
+	<td>{genre}</td>
+</tr>
